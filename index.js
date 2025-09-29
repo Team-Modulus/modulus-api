b1db108e7eeb7536d0cf35fbf959611710961748
jishnuTP <tpjishnu5@gmail.com>
2025-09-29 18:10:38 +0530

    mongo

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.error(err));
 
